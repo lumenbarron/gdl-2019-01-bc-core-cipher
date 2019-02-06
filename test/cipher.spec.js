53ab36a60932682d79f5a9e7d2e024c627bc6f25
lumenbarron <lucemendez@gmail.com>
2019-02-06 16:44:05 -0600

    Actualización del README

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -17,10 +17,10 @@ describe('cipher', () => {
     //
     // Si decides implementar soporte para caracteres no alfabéticos descomenta
     // el test a continuación.
-    //
-    // it('debería retornar " !@" para " !@"', () => {
-    //   assert.equal(cipher.encode(33, ' !@'),' !@');
-    // });
+    
+     it('debería retornar " !@" para " !@"', () => {
+       assert.equal(cipher.encode(33, ' !@'),' !@');
+     });
 
     //
     // Hacker edition
@@ -28,9 +28,9 @@ describe('cipher', () => {
     // Si decides implementar soporte para caracteres no alfabéticos descomenta
     // el test a continuación.
     //
-    // it('debería retornar " !@" para " !@"', () => {
-    //   assert.equal(cipher.encode(33, ' !@'),' !@');
-    // });
+     it('debería retornar " !@" para " !@"', () => {
+       assert.equal(cipher.encode(33, ' !@'),' !@');
+     });
   });
 
   describe('cipher.decode', () => {
@@ -48,12 +48,12 @@ describe('cipher', () => {
     // Si decides agregar soporte para minúsculas descomenta el test a
     // continuación.
     //
-    // it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
-    //   assert.equal(
-    //     cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg'),
-    //     'abcdefghijklmnopqrstuvwxyz'
-    //   );
-    // });
+     it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
+       assert.equal(
+         cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg'),
+         'abcdefghijklmnopqrstuvwxyz'
+       );
+     });
 
     //
     // Hacker edition
@@ -61,9 +61,9 @@ describe('cipher', () => {
     // Si decides implementar soporte para caracteres no alfabéticos descomenta
     // el test a continuación.
     //
-    // it('debería retornar " !@" para " !@"', () => {
-    //   assert.equal(cipher.decode(33, ' !@'),' !@');
-    // });
+     it('debería retornar " !@" para " !@"', () => {
+       assert.equal(cipher.decode(33, ' !@'),' !@');
+     });
   });
 
 });
