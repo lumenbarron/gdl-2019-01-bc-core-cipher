fc0f00884ed46ca80f9a8d21666dd90edf6ecd79
lumenbarron <lucemendez@gmail.com>
2019-02-06 11:49:36 -0600

    Actualización final

--- src/cipher.js
+++ src/cipher.js
@@ -47,13 +47,4 @@ window.cipher = {
 }
 };
 
-/*src/cipher.js: acá debes implementar el objeto cipher, 
-el cual debe estar exportado en el objeto global (window). 
-Este objeto (cipher) debe contener dos métodos:
-cipher.encode(offset, string): offset es el número de posiciones
- que queremos mover a la derecha en el alfabeto y 
- string el mensaje (texto) que queremos cifrar.
-cipher.decode(offset, string): offset es el número de posiciones 
-que queremos mover a la izquierda en el alfabeto y 
-string el mensaje (texto) que queremos descifrar.*/
 
